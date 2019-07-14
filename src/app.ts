dd371a5341b491cad535d63e2b1000f35dcb1c53
Ward Chiang <wardrium@gmail.com>
2019-07-14 12:11:54 -0700

    Project structure cleanup

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { VideoController } from "./controller/video.controller";
+import { VideoController } from "./controllers/VideoController";
 
 const app = express();
 const port = 8081;
 
-app.use('/video/', VideoController);
+app.use('/video', VideoController);
 
 app.listen(port, () => {
    console.log(`server started at http://localhost:${port}`);
