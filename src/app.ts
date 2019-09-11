2fe0d54c59ae6c608dc2aa50d3f6e60c7a2c585e
Ward Chiang <wardrium@gmail.com>
2019-09-11 00:52:44 -0700

    Fix search endpoint to use search string from request

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import express from "express";
 import { createConnection } from "typeorm";
+import bodyParser from "body-parser";
 
 import { VideoController } from "./app/controller/VideoController";
 import { VideoRequestController } from "./app/controller/VideoRequestController";
@@ -8,6 +9,11 @@ import { VideoRequestController } from "./app/controller/VideoRequestController"
 createConnection().then(async connection => {
    const app = express();
    const port = 8081;
+
+   app.use(bodyParser.urlencoded({
+      extended: true
+   }));
+   app.use(bodyParser.json());
 
    app.use("/videos", VideoController);
    app.use("/request", VideoRequestController);
--- src/app/controller/VideoRequestController.ts
+++ src/app/controller/VideoRequestController.ts
@@ -6,12 +6,15 @@ const router: Router = Router();
 
 router.post("/search", async(req, res) => {
     const animeTorrentAdaptor = getAnimeTorrentAdaptor();
-    const searchText = "horriblesubs 720p";
+
+    const body: {
+        search: string;
+    } = req.body;
 
     res.setHeader("Access-Control-Allow-origin", "*");
     res.setHeader("Content-Type", "application/json");
 
-    const results = await animeTorrentAdaptor.search(searchText);
+    const results = await animeTorrentAdaptor.search(body.search);
     res.send(JSON.stringify({
         "results": results
     }));
